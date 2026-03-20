f7716cb740ec3cb88345cdba1fbd45a7d544fe4c
shreyaskumbhar4-hub <shreyaskumbhar4@gmail.com>
2026-03-20 17:05:13 +0530

    second project Number Gasing game) Completed

--- Phase_01_Beginner/02_Number_Guessing/main.cpp
+++ Phase_01_Beginner/02_Number_Guessing/main.cpp
@@ -1,5 +1,36 @@
 # include <iostream>
+#include <cstdlib> // Necessary for rand() and srand()
+#include <ctime>   // Necessary for time()
 using namespace std;
 int main(){
     
+    srand(time(0));// Adding time seed so numbers are different every time
+    int numberStart , numberLast;
+    int guessed_number,random_number;
+
+    cout<<"-----------------------------Number Gasing Game---------------------------------"<<endl;
+    cout<<"----------------------choose the number series for gassing -------------------------"<<endl<<"\t(first number is less than next number)"<<endl;
+    cin>>numberStart>>numberLast;
+    if(numberStart == numberLast || numberStart > numberLast){
+        cout<<"Error your are not coosing the series corectly"<<endl;
+    }else{
+    cout<<"\t-----------------------------Start the number Gasing Game---------------------------------"<<endl;
+    cout<<"\t-----------------------------Coose the number---------------------------------"<<endl;
+    cin>>guessed_number;
+    cout<<"\t--------------------------Result-----------------------"<<endl;
+    random_number= rand() % numberLast - numberStart +1;
+    if(guessed_number > numberLast || guessed_number < numberStart){
+        cout<<"\t the number is out of range";
+    
+    }else{
+        if(guessed_number==random_number){
+            cout<<"Congratulation You Win";
+        }else{
+            cout<<"\tYou lose Wrong choice the correct\t Number was: "<<random_number<<endl;
+        }
+    }
+    }
+    cout<<"\t---------------------------------------------------------------";
+
+    return 0;
 }
