83dc9f6914b45f7acc932854e00dab4342e344cb
shreyaskumbhar4-hub <shreyaskumbhar4@gmail.com>
2026-03-20 18:31:23 +0530

    Add a magic mode

--- Phase_01_Beginner/02_Number_Guessing/main.cpp
+++ Phase_01_Beginner/02_Number_Guessing/main.cpp
@@ -7,24 +7,41 @@ int main(){
     srand(time(0));// Adding time seed so numbers are different every time
     int numberStart , numberLast;
     int guessed_number,random_number;
-
-    cout<<"-----------------------------Number Gasing Game---------------------------------"<<endl;
-    cout<<"----------------------choose the number series for gassing -------------------------"<<endl<<"\t(first number is less than next number)"<<endl;
+    int range ;
+   
+   
+    cout<<"-----------------------------Number Guessing Game---------------------------------"<<endl;
+    cout<<"----------------------choose the number series for guessing -------------------------"<<endl<<"\t(first number is less than next number)"<<endl;
     cin>>numberStart>>numberLast;
+    
+    range = numberLast - numberStart +1;
+    random_number = rand() % range + numberStart;
+    
     if(numberStart == numberLast || numberStart > numberLast){
-        cout<<"Error your are not coosing the series corectly"<<endl;
+        cout<<"Error your are not coosing the series corectly(minimum 1,, maimum = infinit)"<<endl;
     }else{
-    cout<<"\t-----------------------------Start the number Gasing Game---------------------------------"<<endl;
+    cout<<"\t-----------------------------Start the number Guessing Game---------------------------------"<<endl;
     cout<<"\t-----------------------------Coose the number---------------------------------"<<endl;
     cin>>guessed_number;
+    if(guessed_number == 999){
+        cout << "\t[MAGIC]---YOU UNLOCK THE MAGIC MODE"<<endl;
+        cout<<" \tMAGIC Started! (You Typed '999' for MAGIC Spell) ---" << endl;
+        cout<<"\t[Magic] Secret Number is: "<< random_number<<endl;
+        cout<<"\tGuess the number"<<endl;
+        cin>>guessed_number;
+    }
+    else if(guessed_number == 0){
+        guessed_number = 1;
+    }
+
     cout<<"\t--------------------------Result-----------------------"<<endl;
-    random_number= rand() % numberLast - numberStart +1;
+
     if(guessed_number > numberLast || guessed_number < numberStart){
         cout<<"\t the number is out of range";
     
     }else{
         if(guessed_number==random_number){
-            cout<<"Congratulation You Win";
+            cout<<"\tCongratulation You Win"<<endl;
         }else{
             cout<<"\tYou lose Wrong choice the correct\t Number was: "<<random_number<<endl;
         }
@@ -33,4 +50,4 @@ int main(){
     cout<<"\t---------------------------------------------------------------";
 
     return 0;
-}c
+}
